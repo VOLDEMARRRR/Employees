ed33e72638890ef13d86cd36497d2716aa504b75
VOLDEMARRRR <109288257+VOLDEMARRRR@users.noreply.github.com>
2022-07-15 13:51:19 +0400

    Add files via upload

--- Organization.java
+++ Organization.java
@@ -52,6 +52,55 @@ public class Organization {
                 return mas_department[i];
             }
         }
+        return mas_department[0]; // пока хз как красиво выйти, если имя не будет совпадать
     }
 
+    public Department[] getMas_department() {
+        return mas_department;
+    }
+
+    public int countDepartment() {
+        return mas_department.length;
+    }
+
+    public int countEmployee() {
+        int count = 0;
+        for (int i = 0; i < mas_department.length; i++) {
+            count += mas_department[i].getNum_employee();
+        }
+        return count;
+    }
+
+    public int countEmployeePost(String post) {
+        int count = 0;
+        for (int i = 0; i < mas_department.length; i++) {
+            count += mas_department[i].getMasPost(post).length;
+        }
+        return count;
+    }
+
+    public Employee richMan() {
+        int count = 0;
+        Employee rich = null;
+        for (int i = 0; i < mas_department.length; i++) {
+            if (mas_department[i].getMasSortSalary()[0].getSalary() > count) {
+                count = mas_department[i].getMasSortSalary()[0].getSalary();
+                rich = mas_department[i].getMasSortSalary()[0];
+            }
+        }
+        return rich;
+    }
+
+    public Department employeeDepartment(String name, String surname) {
+        Employee[] find_employee;
+        for (int i = 0; i < mas_department.length; i++) {
+            find_employee = mas_department[i].getMas_employee();
+            for (int j = 0; j < find_employee.length ; j++) {
+                if (find_employee[j].getName().equals(name) && find_employee[j].getSurname().equals(surname)) {
+                    return mas_department[i];
+                }
+            }
+        }
+        return mas_department[0]; // также пока хз как норм выйти если проверка не прошла
+    }
 }
